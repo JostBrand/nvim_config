062a1dba88e8d17bd0458e41af8a92129b77b08b
Jost Brandstetter <brandstetterjost@gmail.com>
2023-08-10 18:51:50 +0200

    Added zenmode and lualine

--- plugins/remap.lua
+++ plugins/remap.lua
@@ -59,3 +59,22 @@ vim.keymap.set('n','N',"Nzzzn")
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<c-F>",
   "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+
+
+
+-- zen mode
+local truezen = require('true-zen')
+
+vim.keymap.set('n', '<leader>zn', function()
+  local first = 0
+  local last = vim.api.nvim_buf_line_count(0)
+  truezen.narrow(first, last)
+end, { noremap = true })
+vim.keymap.set('v', '<leader>zn', function()
+  local first = vim.fn.line('v')
+  local last = vim.fn.line('.')
+  truezen.narrow(first, last)
+end, { noremap = true })
+vim.keymap.set('n', '<leader>zf', truezen.focus, { noremap = true })
+vim.keymap.set('n', '<leader>zm', truezen.minimalist, { noremap = true })
+vim.keymap.set('n', '<leader>za', truezen.ataraxis, { noremap = true })
