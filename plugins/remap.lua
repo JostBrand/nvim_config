76bc2704959e25cb9467f438417c1f5d5c3d3d0f
Jost Brandstetter <brandstetterjost@gmail.com>
2023-12-30 13:54:36 +0100

    cleanup unused keybindings

--- plugins/remap.lua
+++ plugins/remap.lua
@@ -1,42 +1,23 @@
 local keyset = vim.keymap.set
 keyset('i', 'qw', '<Esc>', {noremap = true, silent = true})
 
--- Visual mode mapping for indenting using Tab
+-- Visual mode mapping for indenting using Tab and shift tab
 keyset('v', '<Tab>', '>gv', {noremap = true, silent = true})
-
--- Visual mode mapping for un-indenting using Shift-Tab
 keyset('v', '<S-Tab>', '<gv', {noremap = true, silent = true})
 
 -- Map <leader>n to NERDTreeFocus
--- keyset('n', '<leader>n', ':NERDTreeFocus<CR>', {noremap = true, silent = true})
-
--- Map <C-t> to NERDTreeToggle
 keyset('n', '<leader>n', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
 
--- Map <C-f> to NERDTreeFind
-keyset('n', '<C-f>', ':NERDTreeFind<CR>', {noremap = true, silent = true})
-
 keyset('n', '<leader>t', ':TagbarToggle<CR>', {noremap = true, silent = true})
--- Coc keybindings
--- Go to definition
+
+
+
+-- Lsp keybindings
 keyset('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
-
--- Go to references
 keyset('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', {noremap = true, silent = true})
-
--- Show hover
 keyset('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true, silent = true})
-
--- Show signature help
 keyset('n', 'gs', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', {noremap = true, silent = true})
-
--- Rename symbol
 keyset('n', '<leader>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', {noremap = true, silent = true})
-
--- Insert mode mapping for completion
-keyset('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true, noremap = true})
-keyset('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true, noremap = true})
-keyset('i', '<CR>', 'pumvisible() ? "\\<C-y>" : "\\<C-g>u\\<CR>"', {expr = true, noremap = true})
 
 local builtin = require('telescope.builtin')
 keyset('n', '<C-f>',function () builtin.find_files({hidden=true}) end,{})
@@ -52,24 +33,9 @@ keyset('n','<C-u>',"<C-u>zz")
 
 keyset('n','n',"nzzzn")
 keyset('n','N',"Nzzzn")
+
+vim.g.undotree_SetFocusWhenToggle = 1
 keyset('n', '<leader>u', vim.cmd.UndotreeToggle)
-
--- zen mode
-local truezen = require('true-zen')
-
-keyset('n', '<leader>zn', function()
-  local first = 0
-  local last = vim.api.nvim_buf_line_count(0)
-  truezen.narrow(first, last)
-end, { noremap = true })
-keyset('v', '<leader>zn', function()
-  local first = vim.fn.line('v')
-  local last = vim.fn.line('.')
-  truezen.narrow(first, last)
-end, { noremap = true })
-keyset('n', '<leader>zf', truezen.focus, { noremap = true })
-keyset('n', '<leader>zm', truezen.minimalist, { noremap = true })
-keyset('n', '<leader>za', truezen.ataraxis, { noremap = true })
 
 --smart toggle nerdtree so it looks for git or goes into current dir of open buffer
 vim.api.nvim_set_keymap('n', '<C-n>', ':lua require("plugins/nerdtree").nerdtree_smart_toggle()<CR>', { noremap = true, silent = true })
