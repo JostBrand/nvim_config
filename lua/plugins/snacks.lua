05970196cee25652f9b116896fdaa9c735d3e398
Jost Brandstetter <46358368+JostBrand@users.noreply.github.com>
2025-03-31 10:36:08 +0200

    try different dashboard layout; preset from github repo

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,7 +8,36 @@ return {
     -- refer to the configuration section below
     bigfile = { enabled = true },
     notifier = { enabled = true },
-    dashboard = {enabled = true},
+    dashboard = { enabled = true,
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "colorscript -e square",
+          height = 5,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "git status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+    },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
