97139400f1c36e6dc2f9dbfe8f85bfed8d2ce5e1
JostBrand <brandstetterj@airmail.cc>
2025-07-07 19:18:34 +0200

    add hostname check for obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,3 +1,27 @@
+-- Get hostname to conditionally load workspace
+local function get_hostname()
+  local handle = io.popen("hostname")
+  if handle then
+    local result = handle:read("*a")
+    handle:close()
+    return result:gsub("%s+", "") -- Remove whitespace
+  end
+  return ""
+end
+
+local hostname = get_hostname()
+local workspaces = {}
+
+-- Only load workspace if hostname is "desktopmem"
+if hostname == "desktopmeme" then
+  workspaces = {
+    {
+      name = "obsidian_notes",
+      path = "/bkp/obsidian_notes/",
+    },
+  }
+end
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*",  -- recommended, use latest release instead of latest commit
@@ -10,12 +34,7 @@ return {
     -- see below for full list of optional dependencies 👇
   },
   opts = {
-    workspaces = {
-      {
-        name = "obsidian_notes",
-        path = "/bkp/obsidian_notes/",
-      },
-    },
+    workspaces = workspaces,
     -- see below for full list of options 👇
   },
 }
