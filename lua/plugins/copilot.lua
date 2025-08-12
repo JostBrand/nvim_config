4a085d1a879c7ce93f04f30ac956f7a5e370038d
JostBrand <brandstetterj@airmail.cc>
2025-08-12 17:51:21 +0200

    removed unused img-clip

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -43,20 +43,6 @@ return {
             "ibhagwan/fzf-lua",
             "nvim-tree/nvim-web-devicons",
             {
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        use_absolute_path = true,
-                    },
-                },
-            },
-            {
                 'MeanderingProgrammer/render-markdown.nvim',
                 opts = {
                     file_types = { "markdown", "Avante" },
